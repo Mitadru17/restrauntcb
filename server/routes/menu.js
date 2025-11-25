bb0156460dbff42e4a0271cbefa91c7c5e4fe5d2
Mitadru Roy <mitadruroy@Mitadrus-MacBook-Air.local>
2025-11-25 20:09:27 +0530

    Client only branch

--- server/routes/menu.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const menuController = require('../controllers/menuController');
-
-router.get('/', menuController.getMenu);
-
-module.exports = router;
